d932f17836b33de6f9354a656f5cf2d8096456dd
gefalko <karka.edgaras@gmail.com>
2017-04-13 11:16:22 +0300

    Update Options.js

--- src/components/quiz/question/Options.js
+++ src/components/quiz/question/Options.js
@@ -4,32 +4,35 @@ import { RadioGroup, RadioButton } from 'react-toolbox/lib/radio';
 export default class Options extends React.Component{
 
     constructor(props){
+        
         super(props);
         this.state = {value:''};
         this.baseState = this.state;
+        
     }
-
+    
+    //reset state when update props
     componentWillReceiveProps(newProps) {
+        
         this.setState(this.baseState);
+        
     }
-
 
     changeHandle = (value)=>{
+        
         this.setState({value:value});
         this.props.changeHandle(value);
+        
     }
 
     render(){
+        
         const optionsList = this.props.options.map((option, index)=>{
             return (
-                <RadioButton
-                    value={index+''}
-                    label={option}
-               />
+                <RadioButton value={index+''} label={option} />
             )
         });
         
-
         return(
             <RadioGroup  value={this.state.value} onChange={this.changeHandle} >
                 {optionsList}
