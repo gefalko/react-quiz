08f146c407d88c3c8f2c99f4e29d9d28568a253e
gefalko <karka.edgaras@gmail.com>
2017-04-13 12:50:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { overrideComponentTypeChecker } from 'react-toolbox';
-import { Button } from 'react-toolbox/lib/button';
 import App from './components/App'
 
 const rootEl = document.getElementById('app');
