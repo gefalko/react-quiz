707f87a136dc53acec24c8d4bde6ed2611eb12d5
gefalko <karka.edgaras@gmail.com>
2017-04-13 11:06:52 +0300

    remove node neveroment from index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { overrideComponentTypeChecker } from 'react-toolbox';
-import { Button } from 'react-toolbox/lib/button'; // Bundled component import
+import { Button } from 'react-toolbox/lib/button';
 import App from './components/App'
 
 const rootEl = document.getElementById('app');
@@ -13,18 +13,6 @@ const render = () => {
     ,rootEl
   );
 };
-
-if (process.env.NODE_ENV !== 'production') {
-  overrideComponentTypeChecker((classType, reactElement) => (
-    reactElement && (
-      reactElement.type === classType
-      || reactElement.type.name === classType.displayName
-    )
-  ));
-  if (module.hot) {
-    module.hot.accept('./components/App', render);
-  }
-}
 
 render();
 
